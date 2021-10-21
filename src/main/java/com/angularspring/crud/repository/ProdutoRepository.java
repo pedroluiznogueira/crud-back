0b24ef2a71ebe11cf3daa884c8375116179419ef
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 18:15:15 -0300

    Criado o ProdutoRepository

--- src/main/java/com/angularspring/crud/repository/ProdutoRepository.java
+++ src/main/java/com/angularspring/crud/repository/ProdutoRepository.java
@@ -1,4 +1,9 @@
 package com.angularspring.crud.repository;
 
-public class ProdutoRepository {
+import com.angularspring.crud.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
