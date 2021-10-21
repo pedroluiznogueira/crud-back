754f50de0b961d9d0f6cb6e2f65e9500a4eb9caa
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-21 18:34:27 -0300

    Implementadas a requisição retornando todos os produtos

--- src/main/java/com/angularspring/crud/controller/ProdutoController.java
+++ src/main/java/com/angularspring/crud/controller/ProdutoController.java
@@ -1,4 +1,27 @@
 package com.angularspring.crud.controller;
 
+import com.angularspring.crud.model.Produto;
+import com.angularspring.crud.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produto")
+@CrossOrigin("*")
 public class ProdutoController {
+
+    // injeção de dependência
+    @Autowired
+    private ProdutoService produtoService;
+
+    // requisição Get
+    @GetMapping
+    public List<Produto> getAll() {
+        return produtoService.listarProdutos();
+    }
 }
--- src/main/java/com/angularspring/crud/service/ProdutoService.java
+++ src/main/java/com/angularspring/crud/service/ProdutoService.java
@@ -1,4 +1,20 @@
 package com.angularspring.crud.service;
 
+import com.angularspring.crud.model.Produto;
+import com.angularspring.crud.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProdutoService {
+
+    // injeção de dependência
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<Produto> listarProdutos() {
+        return produtoRepository.findAll();
+    }
 }
